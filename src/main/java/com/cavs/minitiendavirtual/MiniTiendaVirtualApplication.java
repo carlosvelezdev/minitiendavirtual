f91d91c3a29d87c84be8ee91a8d469614a96765a
Carlos Velez <carlosvelezcmj@gmail.com>
2025-08-11 09:47:35 -0500

    Initial commit.

---
+++ src/main/java/com/cavs/minitiendavirtual/MiniTiendaVirtualApplication.java
@@ -0,0 +1,13 @@
+package com.cavs.minitiendavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniTiendaVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniTiendaVirtualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cavs/minitiendavirtual/MiniTiendaVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cavs.minitiendavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniTiendaVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
